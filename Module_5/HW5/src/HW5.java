82579f5a40b0d139e7a7ae23e39398fd21a1f725
ai1002hash <glenganderton@proton.me>
2025-03-23 17:58:58 +1100

    Question 3 completed

--- Module_5/HW5/src/HW5.java
+++ Module_5/HW5/src/HW5.java
@@ -1,5 +1,8 @@
 public class HW5 {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println("---Homework 5---");
+        int i = Integer.parseInt(args[0]);
+        int j = Integer.parseInt(args[1]);
+        System.out.printf("%d", i + j);
     }
 }
