916ae13a490d556472d3427c1bcea7923b47af77
ai1002hash <glenganderton@proton.me>
2025-03-23 18:19:48 +1100

    Refactoring files

--- Module_5/HW5/src/HW5.java
+++ Module_5/HW5/src/HW5.java
@@ -1,8 +1,5 @@
 public class HW5 {
     public static void main(String[] args) {
-        System.out.println("---Homework 5---");
-        int i = Integer.parseInt(args[0]);
-        int j = Integer.parseInt(args[1]);
-        System.out.printf("%d", i + j);
+        AddIntegers.main(new String[]{"5", "10"});
     }
 }
