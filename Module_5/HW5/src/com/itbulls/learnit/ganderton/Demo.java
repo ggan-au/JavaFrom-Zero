80f8dfc05c5c3209171c91294ec10ae9a7d99dc6
ai1002hash <glenganderton@proton.me>
2025-03-23 19:56:30 +1100

    Homework complete

--- Module_5/HW5/src/com/itbulls/learnit/ganderton/Demo.java
+++ Module_5/HW5/src/com/itbulls/learnit/ganderton/Demo.java
@@ -6,7 +6,9 @@ import com.itbulls.learnit.operations.CircleCircumference;
 public class Demo {
     public static void main(String[] args) {
         AddIntegers.main(new String[] {"5", "10"});
+        System.out.println();
         AreaOfTriangle.main(new String[] {});
+        System.out.println();
         CircleCircumference.main(new String[] {});
     }
 }
--- Module_5/HW5/src/com/itbulls/learnit/operations/CircleCircumference.java
+++ Module_5/HW5/src/com/itbulls/learnit/operations/CircleCircumference.java
@@ -1,7 +1,13 @@
 package com.itbulls.learnit.operations;
 
+import java.util.Scanner;
+
 public class CircleCircumference {
     public static void main(String[] args) {
-
+        Scanner console = new Scanner(System.in);
+        System.out.println("Enter circle radius: ");
+        double radius = console.nextInt();
+        double circumference = 2 * radius * Math.PI;
+        System.out.printf("%.2f", circumference);
     }
 }
